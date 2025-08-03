dde5e25a8a1224a3e98fafec2ca2c0cd2c23b980
reem-abdelrazek20 <156788337+reem-abdelrazek20@users.noreply.github.com>
2025-08-03 22:12:40 +0300

    Update projecta.jsx

--- src/components/projecta.jsx
+++ src/components/projecta.jsx
@@ -40,13 +40,14 @@ export default function Project() {
          <Card
           title="Platinum Global Motors"
           copy="صفحة هبوط  ."
-          backgroundImage="/img/اimge1.png"
+          backgroundImage="/img/اimge1.jpg"
           link="https://car-zeta-three.vercel.app/"
         />
       </div>
     </section>
   );
 }
+
 
 
 
